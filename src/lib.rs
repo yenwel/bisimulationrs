6c5e80c6bc72ef2112d1f149cef290c0c63da260
Jan Vandepitte <jan.vandepitte@centric.eu>
2017-08-31 09:54:25 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 extern crate carboxyl;
 
 //to do using reactive streams make bisimulation algorithm
+//https://arxiv.org/pdf/1311.7635.pdf
+//http://www.math.unipd.it/~crafa/Pubblicazioni/CrafaRanzatoICALP11.pdf
+//https://arxiv.org/pdf/1705.08362.pdf
 
 
 #[cfg(test)]
