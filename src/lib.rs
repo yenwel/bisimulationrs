6c71f177f816d7cf276a6055627443763346ce96
Jan Vandepitte <Jan.Vandepitte@mediahuis.be>
2018-09-18 15:41:25 +0200

    impl LTS

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,93 @@
 extern crate carboxyl;
+use std::collections::HashMap;
 
-
-trait State {
-
-}
 
 struct LabelledTransitionSystem {
    states : Vec<String>,
-   transitions : Vec<(String,String)>,
-   labeling_function : fn((String,String)) -> String
+   transitions : HashMap<String,(String,String)>
+}
+
+impl LabelledTransitionSystem {
+    fn new(states : Vec<String>,  transitions : HashMap<String,(String,String)>) -> LabelledTransitionSystem
+    {
+        LabelledTransitionSystem {
+            states,
+            transitions
+        }
+    }
+}
+
+fn bisimulates(lts_one : LabelledTransitionSystem, lts_two : LabelledTransitionSystem) -> bool {
+    true
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn bisimilar_for_two_unsimilar_lts() {
+        let states_one = 
+            vec![
+                "born".to_string(), 
+                "dead".to_string(), 
+                "sick".to_string(), 
+                "healthy".to_string()];
+        let transitions_one :  HashMap<String,(String,String)> = 
+        [("disease".to_string(), ("born".to_string(), "sick".to_string())),
+         ("death".to_string(), ("sick".to_string(), "dead".to_string())),
+         ("healing".to_string(), ("sick".to_string(),"healthy".to_string()))
+        ].iter().cloned().collect();
+        let lts_one = LabelledTransitionSystem::new(
+            states_one, transitions_one
+        );
+        let states_two = 
+            vec![
+                "born".to_string(), 
+                "dead".to_string(), 
+                "sick".to_string(), 
+                "healthy".to_string()];       
+        let transitions_two : HashMap<String,(String,String)> = 
+        [("disease".to_string(), ("born".to_string(), "sick".to_string())),
+         ("death".to_string(), ("sick".to_string(), "dead".to_string())),
+         ("healing".to_string(), ("sick".to_string(),"healthy".to_string()))
+        ].iter().cloned().collect();
+        let lts_two = LabelledTransitionSystem::new(
+             states_two, transitions_two
+        );
+        assert!(bisimulates(lts_one,lts_two));
+    }
+    
+    #[test]
+    fn bisimilar_for_two_similar_lts() {
+        let states_one = 
+            vec![
+                "born".to_string(), 
+                "dead".to_string(), 
+                "sick".to_string(), 
+                "healthy".to_string()];
+        let transitions_one :  HashMap<String,(String,String)> = 
+        [("disease".to_string(), ("born".to_string(), "sick".to_string())),
+         ("death".to_string(), ("sick".to_string(), "dead".to_string())),
+         ("healing".to_string(), ("sick".to_string(),"healthy".to_string()))
+        ].iter().cloned().collect();
+        let lts_one = LabelledTransitionSystem::new(
+            states_one, transitions_one
+        );
+        let states_two = 
+            vec![
+                "born".to_string(), 
+                "dead".to_string(), 
+                "sick".to_string(), 
+                "healthy".to_string()];       
+        let transitions_two : HashMap<String,(String,String)> = 
+        [("disease".to_string(), ("born".to_string(), "sick".to_string())),
+         ("death".to_string(), ("sick".to_string(), "dead".to_string())),
+         ("healing".to_string(), ("sick".to_string(),"healthy".to_string()))
+        ].iter().cloned().collect();
+        let lts_two = LabelledTransitionSystem::new(
+             states_two, transitions_two
+        );
+        assert!(!bisimulates(lts_one,lts_two));
     }
 }
